71a0a73a7fa911e3f0faa8f1a443f63ee71d5e11
belanEu <evgeniy.belan.97@mail.ru>
2021-11-05 14:57:58 +0300

    использованы секреты

--- release/constant.mjs
+++ release/constant.mjs
@@ -1,5 +1,5 @@
 export const BASE_URL = 'https://api.tracker.yandex.net/v2/issues';
-export const AUTH_TOKEN = 'AQAAAAA7IE6_AAd46F8KihtUREqBrRXr7eV_5yU';
-export const QUEUE_ID = 6461097;
-export const QUEUE_NAME = 'TMP';
+export const AUTH_TOKEN = process.env.AUTH_TOKEN;
+export const QUEUE_ID = process.env.QUEUE_ID;
+export const QUEUE_NAME = process.env.QUEUE_NAME;
 export const APP='shri-example-store'
